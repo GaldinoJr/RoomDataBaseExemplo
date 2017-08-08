c632f57a53530971c092c50273dd0c836ee0288f
GaldinoMadeInWeb <galdino.ouros@madeinweb.com.br>
2017-08-08 15:23:14 -0300

    link local example

--- app/src/main/java/com/example/galdino/roomdatabaseexemplo/MainActivity.java
+++ app/src/main/java/com/example/galdino/roomdatabaseexemplo/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Retirado o exemplo do link:
+//    https://medium.com/@ajaysaini.official/building-database-with-room-persistence-library-ecf7d0b8f3e9
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
